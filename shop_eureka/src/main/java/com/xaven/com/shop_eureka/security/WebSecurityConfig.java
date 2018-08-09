45d1923b03a93c07757e358fa01e5c13630f57df
xaven <2274420416@qq.com>
2018-08-09 17:38:44 +0800

    添加base认证配置类，解决注册不上的问题

---
+++ shop_eureka/src/main/java/com/xaven/com/shop_eureka/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+/*
+ *
+ *  * Copyright (C), 2002-2014, 苏宁易购电子商务有限公司
+ *  * FileName: WebSecurityConfig.java
+ *  * Author:   18051080
+ *  * Date:     2018年8月9日下午05:26:51
+ *  * Description: //模块目的、功能描述
+ *  * History: //修改记录
+ *  * <author>      <time>      <version>    <desc>
+ *  * 修改人姓名             修改时间            版本号                  描述
+ *
+ */
+
+package com.xaven.com.shop_eureka.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 高版本的丢弃了
+     *
+     * security:
+     *   basic:
+     *    enabled: true
+     *
+     * 配置，应该使用以下方式开启
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        // 如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
